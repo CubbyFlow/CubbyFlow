0a8c3346fd1c086e33eb47d1690433040c39ad0b
Chris Ohk <utilForever@gmail.com>
2018-05-08 20:38:53 +0900

    Upload Travis CI configuration files

---
+++ Scripts/travis_build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+export NUM_JOBS=1
+
+mkdir build
+cd build
+cmake ..
+make
+bin/UnitTests
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    echo "Disabling pip test for macOS"
+else
+    cd ..
+    pip install --user -r requirements.txt
+    pip install --user .
+    python Tests/PythonTests/main.py
+fi
