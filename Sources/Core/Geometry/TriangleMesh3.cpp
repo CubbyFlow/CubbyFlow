26ec51adda22ee6186d59a2b10476e1067e0bb0c
Chris Ohk <utilForever@gmail.com>
2018-06-09 15:55:22 +0900

    [ci skip] Apply tinyobj library (WIP)

--- Sources/Core/Geometry/TriangleMesh3.cpp
+++ Sources/Core/Geometry/TriangleMesh3.cpp
@@ -12,6 +12,7 @@
 #include <Core/Utils/Parallel.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
+#define TINYOBJLOADER_USE_DOUBLE
 #include <tiny_obj_loader.h>
 
 #include <cassert>
@@ -609,9 +610,145 @@ bool TriangleMesh3::WriteObj(const std::string& fileName) const
 
 bool TriangleMesh3::ReadObj(std::istream* stream)
 {
-    (void)stream;
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+
+    std::string err;
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, stream);
+
+    // `err` may contain warning message.
+    if (!err.empty())
+    {
+        std::cerr << err << '\n';
+        return false;
+    }
+
+    // Failed to load obj.
+    if (!ret)
+    {
+        return false;
+    }
 
     InvalidateBVH();
+
+    // Loop over shapes
+    for (auto& shape : shapes)
+    {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+
+        for (size_t f = 0; f < shape.mesh.num_face_vertices.size(); f++)
+        {
+            size_t fv = shape.mesh.num_face_vertices[f];
+
+            if (fv == 3)
+            {
+                // AddPointTriangle(
+                //    {shape.mesh.indices[index_offset].vertex_index,
+                //     shape.mesh.indices[index_offset + 1].vertex_index,
+                //     shape.mesh.indices[index_offset + 2].vertex_index});
+
+                //AddPointNormalTriangle(
+                //    {shape.mesh.indices[index_offset].vertex_index,
+                //     shape.mesh.indices[index_offset + 1].vertex_index,
+                //     shape.mesh.indices[index_offset + 2].vertex_index},
+                //    {shape.mesh.indices[index_offset].normal_index,
+                //     shape.mesh.indices[index_offset + 1].normal_index,
+                //     shape.mesh.indices[index_offset + 2].normal_index});
+
+                // AddPointUVTriangle(
+                //    {shape.mesh.indices[index_offset].vertex_index,
+                //     shape.mesh.indices[index_offset + 1].vertex_index,
+                //     shape.mesh.indices[index_offset + 2].vertex_index},
+                //    {shape.mesh.indices[index_offset].texcoord_index,
+                //     shape.mesh.indices[index_offset + 1].texcoord_index,
+                //     shape.mesh.indices[index_offset + 2].texcoord_index});
+
+                // AddPointUVNormalTriangle(
+                //    {shape.mesh.indices[index_offset].vertex_index,
+                //     shape.mesh.indices[index_offset + 1].vertex_index,
+                //     shape.mesh.indices[index_offset + 2].vertex_index},
+                //    {shape.mesh.indices[index_offset].texcoord_index,
+                //     shape.mesh.indices[index_offset + 1].texcoord_index,
+                //     shape.mesh.indices[index_offset + 2].texcoord_index},
+                //    {shape.mesh.indices[index_offset].normal_index,
+                //     shape.mesh.indices[index_offset + 1].normal_index,
+                //     shape.mesh.indices[index_offset + 2].normal_index});
+            }
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++)
+            {
+                // access to vertex
+                tinyobj::index_t idx = shape.mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+                tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+                tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+                //tinyobj::real_t nx = attrib.normals[3 * idx.normal_index + 0];
+                //tinyobj::real_t ny = attrib.normals[3 * idx.normal_index + 1];
+                //tinyobj::real_t nz = attrib.normals[3 * idx.normal_index + 2];
+                // tinyobj::real_t tx =
+                //    attrib.texcoords[2 * idx.texcoord_index + 0];
+                // tinyobj::real_t ty =
+                //    attrib.texcoords[2 * idx.texcoord_index + 1];
+
+                AddPoint({vx, vy, vz});
+                // AddNormal({nx, ny, nz});
+                // AddUV({tx, ty});
+            }
+
+            index_offset += fv;
+        }
+    }
+
+    for (auto& shape : shapes)
+    {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+
+        for (size_t f = 0; f < shape.mesh.num_face_vertices.size(); f++)
+        {
+            size_t fv = shape.mesh.num_face_vertices[f];
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++)
+            {
+                // access to vertex
+                tinyobj::index_t idx = shape.mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+                tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+                tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+
+                AddNormal({vx, vy, vz});
+            }
+
+            index_offset += fv;
+        }
+    }
+    for (auto& shape : shapes)
+    {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+
+        for (size_t f = 0; f < shape.mesh.num_face_vertices.size(); f++)
+        {
+            size_t fv = shape.mesh.num_face_vertices[f];
+
+            if (fv == 3)
+            {
+                AddPointNormalTriangle(
+                    {shape.mesh.indices[index_offset].vertex_index,
+                     shape.mesh.indices[index_offset + 1].vertex_index,
+                     shape.mesh.indices[index_offset + 2].vertex_index},
+                    {shape.mesh.indices[index_offset].normal_index,
+                     shape.mesh.indices[index_offset + 1].normal_index,
+                     shape.mesh.indices[index_offset + 2].normal_index});
+            }
+
+            index_offset += fv;
+        }
+    }
 
     return true;
 }
