6dc07533f001c148ab0399665665c9f6d1951f4c
snowapril <sinjihng@naver.com>
2020-06-27 11:09:15 +0000

    feat : revert to camera inhertiance architecture.
    add orthogonal projection camera

--- Experimental/Includes/CubbyRender/Framework/View/PerspectiveCamera.h
+++ Experimental/Includes/CubbyRender/Framework/View/PerspectiveCamera.h
@@ -1,19 +1,19 @@
 /*************************************************************************
-> File Name: Camera.h
+> File Name: PerspectiveCamera.h
 > Project Name: CubbyFlow
 > This code is based on Jet Framework that was created by Doyub Kim.
 > References: https://github.com/doyubkim/fluid-engine-dev
-> Purpose: The camera class which view simulator scene
+> Purpose: The perspective camera derived from camera class.
 > Created Time: 2020/04/16
 > Copyright (c) 2020, Ji-Hong snowapril
 *************************************************************************/
-#ifndef CUBBYFLOW_CAMERA_H
-#define CUBBYFLOW_CAMERA_H
+#ifndef CUBBYFLOW_PERSPECTIVE_CAMERA_H
+#define CUBBYFLOW_PERSPECTIVE_CAMERA_H
 
 #include <Framework/Utils/Prerequisites.h>
-#include <Framework/View/Pivot.h>
-#include <Framework/View/Viewport.h>
+#include <Framework/View/Camera.h>
 #include <Core/Matrix/Matrix4x4.h>
+#include <Core/Utils/Constants.h>
 #include <memory>
 
 namespace CubbyFlow {
@@ -22,36 +22,32 @@ namespace CubbyRender {
     //!
     //! \brief The camera class which view simulator scene
     //!
-    class Camera 
+    class PerspectiveCamera : public Camera 
     {
     public:
         //! Default constructor.
-        Camera();
+        PerspectiveCamera();
 
-        //! constructor with pivot.
-        Camera(const Pivot& pivot);
+        //! constructor with cameraState.
+        PerspectiveCamera(const Pivot& pivot, float fov);
 
         //! Default destructor.
-        virtual ~Camera();
-
-        //! Get camera view transition matrix.
-        Matrix4x4F getViewMatrix() const;
+        ~PerspectiveCamera();
 
         //! Get perspective projection matrix.
-        virtual Matrix4x4F getProjectionMatrix() const = 0;
+        Matrix4x4F getProjectionMatrix() const override;
 
-        //! Get the mutable reference of the camera state
-        Pivot& getPivot();
+        //! Set field of view value.
+        inline void setFov(float fov);
 
-        //! Get the immutable reference of the camera state.
-        const Pivot& getPivot() const;
-
+        //! Get field of view value.
+        inline float getFov() const;
     protected:
-        Pivot _pivot;
+        float _fov = 60.0f;
     private:
     };
 
-    using CameraPtr = std::shared_ptr<Camera>;
+    using PerspectiveCameraPtr = std::shared_ptr<PerspectiveCamera>;
 } 
 }
 
--- Experimental/Sources/CubbyRender/Framework/View/Camera.cpp
+++ Experimental/Sources/CubbyRender/Framework/View/Camera.cpp
@@ -9,7 +9,6 @@
 *************************************************************************/
 
 #include <Framework/View/Camera.h>
-#include <Framework/Utils/Common.h>
 
 namespace CubbyFlow {
 namespace CubbyRender {
@@ -50,82 +49,15 @@ namespace CubbyRender {
         return view;
     }
 
-    Matrix4x4F Camera::getProjectionMatrix() const
-    {
-        Matrix4x4F projection(0.0f);
-        if (_method == ProjectionMethod::PERSPECTIVE)
-        {
-            //! http://www.songho.ca/opengl/gl_projectionmatrix.html
-            //! https://github.com/g-truc/glm/blob/master/glm/ext/matrix_clip_space.inl
-            const float aspect = static_cast<float>(_viewport.getWidth()) / _viewport.getHeight();
-            const float fovTan = 1.0f / std::tan((PI_FLOAT * _fovy / 180.0f) / 2.0f);
-
-		    projection(0, 0) = fovTan / aspect;
-		    projection(1, 1) = fovTan;
-		    projection(2, 2) = -(_zFar - _zNear) / (_zFar + _zNear);
-		    projection(2, 3) = -1.0f;
-		    projection(3, 2) = -2.0f * _zFar * _zNear / (_zFar - _zNear);
-        }
-        else if (_method == ProjectionMethod::ORTHOGONAL)
-        {
-            //! Do nothing.
-        }
-        else
-        {
-            CUBBYFLOW_ERROR << "Unknown Camera Projection Method";
-            std::abort();
-        }
-        return projection;
-    }
-    //! several setter and getter below.
-    void Camera::setProjectionMethod(ProjectionMethod method)
-    {
-        _method = method;
-    }
-    void Camera::setViewport(Viewport viewport)
-    {
-        _viewport = viewport;
-    }
-    void Camera::setNearFar(float near, float far)
-    {
-        _zNear = near;
-        _zFar = far;
-    }
-    void Camera::setFovy(float fovy)
-    {
-        _fovy = fovy;
-    }
-    ProjectionMethod Camera::getProjectionMethod() const
-    {
-        return _method;
-    }
     Pivot& Camera::getPivot()
     {
         return _pivot;
     }
-    Viewport& Camera::getViewport()
-    {
-        return _viewport;
-    }
+
     const Pivot& Camera::getPivot() const
     {
         return _pivot;
     }
-    const Viewport& Camera::getViewport() const
-    {
-        return _viewport;
-    }
-    float Camera::getNear() const
-    {
-        return _zNear;
-    }
-    float Camera::getFar() const
-    {
-        return _zFar;
-    }
-    float Camera::getFovy() const
-    {
-        return _fovy;
-    }
+
 } 
 }
