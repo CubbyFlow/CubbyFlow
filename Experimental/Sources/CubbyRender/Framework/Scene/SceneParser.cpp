991a4050a55aefe586d780e3658cbb42e73d7bca
snowapril <sinjihng@naver.com>
2020-07-20 08:01:36 +0000

    feat : add scene parsing on material ongoing.

--- Experimental/Sources/CubbyRender/Framework/Scene/SceneParser.cpp
+++ Experimental/Sources/CubbyRender/Framework/Scene/SceneParser.cpp
@@ -58,11 +58,39 @@ namespace CubbyRender {
     template <>
     void SceneParser::onLoadSceneObject<PointsRenderable>(const nlohmann::json& json)
     {
-        UNUSED_VARIABLE(json);
-        //! const std::string& dir = json["dir"].get<std::string>();
-        //! const std::string& format = json["format"].get<std::string>();
-        //! const int numPointsCache = json["count"].get<int>();
-        //!_metadata.emplace(name, std::make_shared<TriangleMeshRenderable>(positions, normals));
+        const std::string& name = json["name"].get<std::string>();
+        const std::string& dir = json["dir"].get<std::string>();
+        const std::string& format = json["format"].get<std::string>();
+        const size_t numPointsCache = json["count"].get<size_t>();    
+
+        std::string fileformat = pystring::os::path::join(dir, format);
+        
+        GeometryCacheParserPtr parser = std::make_shared<GeometryCacheParser>();
+        parser->loadGeometryCache(fileformat, numPointsCache);
+        
+        const float scale = json["scale"].get<float>();
+        const Vector3F translation(json["translation"][0].get<float>(), json["translation"][1].get<float>(), json["translation"][2].get<float>());
+
+        const Matrix4x4F st = Matrix4x4F::MakeScaleMatrix(Vector3F(scale, scale, scale)) * Matrix4x4F::MakeTranslationMatrix(translation);
+
+        for (size_t i = 0; i < parser->getNumberOfGeometryCache(); ++i)
+        {
+            auto vertices = parser->getVertexCache(i);
+            const size_t numberOfFloats = VertexHelper::getNumberOfFloats(VertexFormat::Position3Normal3);
+            const size_t numVertices = static_cast<size_t>(vertices.size() / numberOfFloats);
+            ParallelFor(ZERO_SIZE, numVertices, [&](size_t index){
+                const size_t baseIndex = index * numberOfFloats;
+                Vector4F pos = st * Vector4F(vertices[baseIndex + 0], vertices[baseIndex + 1], vertices[baseIndex + 2], 1.0f);
+                vertices[baseIndex]     = pos.x;
+                vertices[baseIndex + 1] = pos.y;
+                vertices[baseIndex + 2] = pos.z;
+            });
+        }
+        
+        const float radius = json["radius"].get<float>();
+
+        _metadata.emplace(name + std::string("Cache"), parser);
+        _metadata.emplace(name, std::make_shared<PointsRenderable>(parser->getVertexCache(0), Vector3F(221 / 255.0f, 145 / 255.0f, 48 / 255.0f), radius));
     }
 
     template <>
@@ -151,6 +179,11 @@ namespace CubbyRender {
         const std::string name = json["name"].get<std::string>();
         _metadata.emplace(name, std::make_shared<Light>(pivot));
     }
+    template <>
+    void SceneParser::onLoadSceneObject<Material>(const nlohmann::json& json)
+    {
+        UNUSED_VARIABLE(json);
+    }
 
     void SceneParser::onLoadScene(const nlohmann::json& json)
     {
@@ -162,6 +195,7 @@ namespace CubbyRender {
             else if (objectType == "object") onLoadSceneObject<TriangleMeshRenderable>(data);
             else if (objectType == "camera") onLoadSceneObject<Camera>(data);
             else if (objectType == "light") onLoadSceneObject<Light>(data);
+            else if (objectType == "material") onLoadSceneObject<Material>(data);
             //! else if (objectType == "render") parseSceneObject<RenderSetting>(data);
             else 
             {
