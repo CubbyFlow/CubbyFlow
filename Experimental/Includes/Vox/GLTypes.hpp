720da7364ff7a01efce8b9b9995859f6a7036922
snowapril <sinjihng@naver.com>
2020-07-26 10:43:00 +0000

    feat : type aliasing with GLtypes

---
+++ Experimental/Includes/Vox/GLTypes.hpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+> File Name: GLTypes.hpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: include frequently used GL types
+> Created Time: 2020/07/26
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#ifndef CUBBYFLOW_VOX_GL_TYPES_HPP
+#define CUBBYFLOW_VOX_GL_TYPES_HPP
+
+
+using GLchar = char;
+using GLenum = unsigned int;
+using GLboolean = unsigned char;
+using GLbitfield = unsigned int;
+using GLbyte = signed char;
+using GLshort = short;
+using GLint = int;
+using GLsizei = int;
+using GLubyte = unsigned char;
+using GLushort = unsigned short;
+using GLuint = unsigned int;
+using GLhalf = unsigned short;
+using GLfloat = float;
+using GLclampf = float;
+using GLdouble = double;
+using GLclampd = double;
+using GLvoid = void;
+
+#endif 
