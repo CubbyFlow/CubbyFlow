035f7566211c4ce13d3f3d8ce8c9ad6df58f0df1
snowapril <sinjihng@naver.com>
2020-07-27 07:59:28 +0000

    feat : add print stack util function
    in linux, we need libunwind dependency

---
+++ Experimental/Includes/Vox/DebugUtils.hpp
@@ -0,0 +1,36 @@
+/*************************************************************************
+> File Name: DebugUtils.hpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> References: OpenGL-Insights Sample chapter
+> Purpose: Debug utility function collection
+> Created Time: 2020/07/26
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#ifndef CUBBYFLOW_VOX_DEBUG_UTILS_HPP
+#define CUBBYFLOW_VOX_DEBUG_UTILS_HPP
+
+#include <Core/Utils/Macros.h>
+#include<Vox/GLTypes.hpp>
+#include<string>
+
+#if defined(CUBBYFLOW_LINUX)
+	extern const char *__progname;
+#endif
+
+namespace Vox {
+    //! The function to print the call stack
+    class StackTrace
+    {
+    public:
+        #if (defined(CUBBYFLOW_WINDOWS) || defined(CUBBYFLOW_LINUX))
+    	static void PrintStack();
+        #else
+        static void PrintStack() {};
+        #endif
+    };
+    
+};
+
+#endif
