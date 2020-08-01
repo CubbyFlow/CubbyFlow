2986dac525d478428d65f79b98d09cbc1433eb89
snowapril <sinjihng@naver.com>
2020-08-01 11:23:32 +0000

    feat : Add GLuint64

--- Experimental/Includes/Vox/GLTypes.hpp
+++ Experimental/Includes/Vox/GLTypes.hpp
@@ -10,23 +10,25 @@
 #ifndef CUBBYFLOW_VOX_GL_TYPES_HPP
 #define CUBBYFLOW_VOX_GL_TYPES_HPP
 
+#include <cstddef>
 
-using GLchar = char;
-using GLenum = unsigned int;
-using GLboolean = unsigned char;
+using GLchar     = char;
+using GLenum     = unsigned int;
+using GLboolean  = unsigned char;
 using GLbitfield = unsigned int;
-using GLbyte = signed char;
-using GLshort = short;
-using GLint = int;
-using GLsizei = int;
-using GLubyte = unsigned char;
-using GLushort = unsigned short;
-using GLuint = unsigned int;
-using GLhalf = unsigned short;
-using GLfloat = float;
-using GLclampf = float;
-using GLdouble = double;
-using GLclampd = double;
-using GLvoid = void;
+using GLbyte     = signed char;
+using GLshort    = short;
+using GLint      = int;
+using GLsizei    = int;
+using GLubyte    = unsigned char;
+using GLushort   = unsigned short;
+using GLuint     = unsigned int;
+using GLuint64   = size_t;
+using GLhalf     = unsigned short;
+using GLfloat    = float;
+using GLclampf   = float;
+using GLdouble   = double;
+using GLclampd   = double;
+using GLvoid     = void;
 
 #endif 
