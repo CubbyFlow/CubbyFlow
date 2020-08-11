57841a15a12ccbe9d856d929e7be4ddb62cff13f
snowapril <sinjihng@naver.com>
2020-08-11 05:32:17 +0000

    feat : Solve invisible floor grid bug
    Cause of the invisible floor bug was "unsigned computation".
    I was trying to do compute below eq.
    -uSize * fSize;
    
    As I attach sign at the unsigned variable, it converted to very large
    unsigned value. As a result, grid vertex positions are out of the view
    frustum. It was my fault.

---
+++ Experimental/Sources/Vox/DebugDraw.cpp
@@ -0,0 +1,145 @@
+/*************************************************************************
+> File Name: DebugDraw.cpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: The debug drawing class for visualizing horizontal grid, x,y,z axis, bounding boxes.
+> Created Time: 2020/08/08
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#include <Vox/DebugDraw.hpp>
+#include <Vox/ShaderPreset.hpp>
+#include <Vox/Renderer.hpp>
+#include <Vox/FrameContext.hpp>
+#include <Vox/Program.hpp>
+#include <glad/glad.h>
+#include <cstring>
+#include <cstdlib>
+
+#include <iostream>
+namespace Vox {
+
+    namespace Detail {
+        GLuint GenerateVAO(const void* data, const size_t numBytes)
+        {
+            GLuint vao, vbo;
+            glGenVertexArrays(1, &vao);
+            glBindVertexArray(vao);
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, numBytes, data, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+            glBindVertexArray(0);
+            return vao;
+        }
+    }
+
+    DebugDraw::DebugDraw()
+    {
+    }
+
+    DebugDraw::~DebugDraw()
+    {
+        for (auto renderable : _renderables)
+            glDeleteVertexArrays(1, &(renderable.vao));
+    }
+
+    void DebugDraw::Initialize(const std::shared_ptr<FrameContext>& ctx)
+    {
+        GLuint vs = Renderer::CreateShaderFromSource(kDebugDrawShaders[0], GL_VERTEX_SHADER);
+        GLuint fs = Renderer::CreateShaderFromSource(kDebugDrawShaders[1], GL_FRAGMENT_SHADER);
+        GLuint program = Renderer::CreateProgram(vs, 0, fs);
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+        ctx->AddShaderProgram("DebugDraw", program);
+    }
+
+    void DebugDraw::AddFloorGrid(const CubbyFlow::Vector2I numCell, float cellSize, const CubbyFlow::Vector3F color)
+    {
+        std::vector<CubbyFlow::Vector3F> positions((numCell.x + numCell.y + 2) * 2);
+
+        //! assume cell outter lines' width is almost equal to 0.
+        const float horizontalLength = numCell.x * cellSize;
+        const float verticalLength = numCell.y * cellSize;
+        const CubbyFlow::Vector2F minCorner = { -numCell.x * cellSize * 0.5f, -numCell.y * cellSize * 0.5f };
+
+        //! Add vertical lines.
+        for (int x = 0; x < numCell.x + 1; ++x)
+        {
+            positions[x * 2] = {minCorner.x + x * cellSize, 0.0f, minCorner.y};
+            positions[x * 2 + 1] = {minCorner.x + x * cellSize, 0.0f, minCorner.y + verticalLength};
+        }
+
+        //! Add horizontal lines.
+        for (int y = 0, baseIdx = (numCell.x + 1) * 2; y < numCell.y + 1; ++y)
+        {
+            positions[baseIdx + y * 2] = {minCorner.x, 0.0f, minCorner.y + y * cellSize};
+            positions[baseIdx + y * 2 + 1] = {minCorner.x + horizontalLength, 0.0f, minCorner.y + y * cellSize};
+        }
+
+        const size_t numBytes = positions.size() * sizeof(CubbyFlow::Vector3F);
+        const size_t numVertices = numBytes / (sizeof(float) * 3);
+        _renderables.push_back({Detail::GenerateVAO(static_cast<void*>(positions.data()), numBytes), numVertices, color});
+    }
+
+    void DebugDraw::AddCartesianCoordinateAxis(const float axisXLength, const float axisYLength, const float axisZLength)
+    {
+        const float axisX[] = {
+            0.0f, 0.0f, 0.0f, axisXLength, 0.0f, 0.0f
+        };
+        const float axisY[] = {
+            0.0f, 0.0f, 0.0f, 0.0f, axisYLength, 0.0f
+        };
+        const float axisZ[] = {
+            0.0f, 0.0f, 0.0f, 0.0f, 0.0f, axisZLength
+        };
+
+        _renderables.push_back({ Detail::GenerateVAO(static_cast<const void*>(axisX), sizeof(axisX)), size_t(2), CubbyFlow::Vector3F(1.0f, 0.0f, 0.0f)});
+        _renderables.push_back({ Detail::GenerateVAO(static_cast<const void*>(axisY), sizeof(axisY)), size_t(2), CubbyFlow::Vector3F(0.0f, 1.0f, 0.0f)});
+        _renderables.push_back({ Detail::GenerateVAO(static_cast<const void*>(axisZ), sizeof(axisZ)), size_t(2), CubbyFlow::Vector3F(0.0f, 0.0f, 1.0f)});
+    }
+
+    void DebugDraw::AddBoundingBox(const CubbyFlow::Vector3F bbMin, const CubbyFlow::Vector3F bbMax, const CubbyFlow::Vector3F color)
+    {
+        const float positions[] = {
+            bbMin.x, bbMin.y, bbMin.z, bbMax.x, bbMin.y, bbMin.z,
+            bbMin.x, bbMin.y, bbMax.z, bbMax.x, bbMin.y, bbMax.z,
+            bbMin.x, bbMax.y, bbMin.z, bbMax.x, bbMax.y, bbMin.z,
+            bbMin.x, bbMax.y, bbMax.z, bbMax.x, bbMax.y, bbMax.z,
+            bbMin.x, bbMin.y, bbMin.z, bbMin.x, bbMin.y, bbMax.z,
+            bbMax.x, bbMin.y, bbMin.z, bbMax.x, bbMin.y, bbMax.z,
+            bbMin.x, bbMax.y, bbMin.z, bbMin.x, bbMax.y, bbMax.z,
+            bbMax.x, bbMax.y, bbMin.z, bbMax.x, bbMax.y, bbMax.z,
+            bbMin.x, bbMin.y, bbMin.z, bbMin.x, bbMax.y, bbMin.z,
+            bbMax.x, bbMin.y, bbMin.z, bbMax.x, bbMax.y, bbMin.z,
+            bbMin.x, bbMin.y, bbMax.z, bbMin.x, bbMax.y, bbMax.z,
+            bbMax.x, bbMin.y, bbMax.z, bbMax.x, bbMax.y, bbMax.z,
+        };
+
+         const size_t numBytes = sizeof(positions);
+         const size_t numVertices = numBytes / (sizeof(float) * 3);
+        _renderables.push_back({ Detail::GenerateVAO(static_cast<const void*>(positions), numBytes), numVertices, color });
+    }
+
+    void DebugDraw::ClearVAO()
+    {
+        for (auto renderable : _renderables)
+            glDeleteVertexArrays(1, &(renderable.vao));
+        _renderables.clear();
+    }
+
+    void DebugDraw::DrawFrame(const std::shared_ptr<FrameContext>& ctx) const
+    {
+        std::weak_ptr<Program> program = ctx->GetCurrentProgram();
+        for (const auto& renderable : _renderables)
+        {
+            if (!program.expired())
+                program.lock()->SendUniformVariable("Color", renderable.color);
+            glBindVertexArray(renderable.vao);
+            glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(renderable.numVertices));
+        }
+        glBindVertexArray(0);
+    }
+
+};
