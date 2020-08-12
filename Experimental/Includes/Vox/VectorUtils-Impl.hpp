fceb5a334f22fcd433327612924bc9a596c115fa
snowapril <sinjihng@naver.com>
2020-08-12 10:03:51 +0000

    feat : Add Vector Related Useful Util Functions.
    1. CheckTriangle function returns whether if given three vertices
    arguments can construct triangle or not.
    
    2. CalculateNormal function returns the normal vector of given three
    vertices.

---
+++ Experimental/Includes/Vox/VectorUtils-Impl.hpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+> File Name: VectorUtils-Impl.h
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: Calculation utilities realted to vector. (ex. calculate normal of three vertices).
+> Created Time: 2020/04/04
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#ifndef CUBBYFLOW_VOX_VECTOR_UTILS_IMPL_HPP
+#define CUBBYFLOW_VOX_VECTOR_UTILS_IMPL_HPP
+
+using namespace CubbyFlow;
+namespace Vox {
+
+    template <typename T, size_t N>
+    bool CheckTriangle(const Vector<T, N>& v1, const Vector<T, N>& v2, const Vector<T, N>& v3)
+    {
+        static_assert(2 <= N && N <= 4); //! At compile time, make sure the dimension of the vertex is in between 2 and 4.
+        return (v2.x - v1.x) * (v3.y - v2.y) == (v3.x - v2.x) * (v2.y - v1.y);
+    }
+
+    template <typename T, size_t N>
+    Vector<T, N> CalculateNormal(const Vector<T, N>& v1, const Vector<T, N>& v2, const Vector<T, N>& v3)
+    {
+        static_assert(2 <= N && N <= 3);
+
+        if (!CheckTriangle(v1, v2, v3))
+            return Vector<T, N>();
+
+        Vector<T, N> edge1 = v2 - v1;
+        Vector<T, N> edge2 = v3 - v2;
+
+        return edge1.Cross(edge2).Normalized();
+    }
+}
+
+#endif
---
+++ Experimental/Includes/Vox/VectorUtils.hpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+> File Name: VectorUtils.hpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: Calculation utilities realted to vector. (ex. calculate normal of three vertices).
+> Created Time: 2020/04/04
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#ifndef CUBBYFLOW_VOX_VECTOR_UTILS_HPP
+#define CUBBYFLOW_VOX_VECTOR_UTILS_HPP
+
+#include <Core/Vector/Vector.h>
+
+namespace Vox {
+
+	//!
+	//! \brief Return given combination of the vertices can be triangle or not.
+	//!
+	//! Simply compute two slopes of the line v1~v2 and v2~v3.
+	//! If computed two slopes are equal, it means v1, v2 and v3 are on the same line.
+	//! ,e.g it cant be a triangle.
+	//! Otherwise they can be a triangle.
+	//! \param v1  a vertex of the triangle
+	//! \param v2  a vertex of the triangle
+	//! \param v3  a vertex of the triangle
+	//!
+    template <typename T, size_t N>
+	bool CheckTriangle(const CubbyFlow::Vector<T, N>& v1, const CubbyFlow::Vector<T, N>& v2, const CubbyFlow::Vector<T, N>& v3);
+
+    //!
+	//! \brief Return Normal Vector of the given three vectors.
+	//!
+	//! This Util function calculate two edge from the three given vertices.
+    //! Calculate cross product of the two edge and return.
+	//! 
+    //! **Three given vertices must be able to construct the triangle.**
+    //!
+	//! \code{.cpp}
+	//! Vector3F v1, v2, v3;
+    //! //! insert data to v1, v2, v3 ...
+    //! Vector3F normal = calculateNormal(v1, v2, v3);
+	//! \endcode
+	//!
+	//! \param v1  a vertex of the triangle
+	//! \param v2  a vertex of the triangle
+	//! \param v3  a vertex of the triangle
+	//!
+    template <typename T, size_t N>
+    CubbyFlow::Vector<T, N> CalculateNormal(const CubbyFlow::Vector<T, N>& v1, const CubbyFlow::Vector<T, N>& v2, const CubbyFlow::Vector<T, N>& v3);
+}
+
+#include <Vox/VectorUtils-Impl.hpp>
+
+#endif
