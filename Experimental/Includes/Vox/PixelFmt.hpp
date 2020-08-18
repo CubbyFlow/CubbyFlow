46fbe366cc9a2382519e98c58d53aed167d8af11
snowapril <sinjihng@naver.com>
2020-08-18 11:05:37 +0000

    feat : Modify header file comments

--- Experimental/Includes/Vox/PixelFmt.hpp
+++ Experimental/Includes/Vox/PixelFmt.hpp
@@ -3,7 +3,7 @@
 > Project Name: CubbyFlow
 > This code is based on Jet Framework that was created by Doyub Kim.
 > References: https://github.com/doyubkim/fluid-engine-dev
-> Purpose: Simple filesystem class
+> Purpose: Pixel format enumeration for texture and buffer attributes.
 > Created Time: 2020/07/29
 > Copyright (c) 2020, Ji-Hong snowapril
 *************************************************************************/
