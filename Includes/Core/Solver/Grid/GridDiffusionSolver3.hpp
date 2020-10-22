b0cead38fcd29b08f3acb83e0e51e7aff24e64a1
Chris Ohk <utilForever@gmail.com>
2020-10-22 13:44:07 +0900

    feat(quality): Improve code quality
    - Add deleted copy/move constructor
    - Add deleted copy/move assignment operator

--- Includes/Core/Solver/Grid/GridDiffusionSolver3.hpp
+++ Includes/Core/Solver/Grid/GridDiffusionSolver3.hpp
@@ -31,10 +31,22 @@ class GridDiffusionSolver3
 {
  public:
     //! Default constructor.
-    GridDiffusionSolver3();
+    GridDiffusionSolver3() = default;
 
-    //! Default destructor.
-    virtual ~GridDiffusionSolver3();
+    //! Deleted copy constructor.
+    GridDiffusionSolver3(const GridDiffusionSolver3&) = delete;
+
+    //! Deleted move constructor.
+    GridDiffusionSolver3(GridDiffusionSolver3&&) noexcept = delete;
+
+    //! Default virtual destructor.
+    virtual ~GridDiffusionSolver3() = default;
+
+    //! Deleted copy assignment operator.
+    GridDiffusionSolver3& operator=(const GridDiffusionSolver3&) = delete;
+
+    //! Deleted move assignment operator.
+    GridDiffusionSolver3& operator=(GridDiffusionSolver3&&) noexcept = delete;
 
     //!
     //! Solves diffusion equation for a scalar field.
@@ -48,10 +60,11 @@ class GridDiffusionSolver3
     //!
     virtual void Solve(const ScalarGrid3& source, double diffusionCoefficient,
                        double timeIntervalInSeconds, ScalarGrid3* dest,
-                       const ScalarField3& boundarySDF = ConstantScalarField3(
-                           std::numeric_limits<double>::max()),
-                       const ScalarField3& fluidSDF = ConstantScalarField3(
-                           -std::numeric_limits<double>::max())) = 0;
+                       const ScalarField3& boundarySDF =
+                           ConstantScalarField3{
+                               std::numeric_limits<double>::max() },
+                       const ScalarField3& fluidSDF = ConstantScalarField3{
+                           -std::numeric_limits<double>::max() }) = 0;
 
     //!
     //! Solves diffusion equation for a collocated vector field.
@@ -63,14 +76,13 @@ class GridDiffusionSolver3
     //! \param boundarySDF Shape of the solid boundary that is empty by default.
     //! \param fluidSDF Shape of the fluid boundary that is full by default.
     //!
-    virtual void Solve(const CollocatedVectorGrid3& source,
-                       double diffusionCoefficient,
-                       double timeIntervalInSeconds,
-                       CollocatedVectorGrid3* dest,
-                       const ScalarField3& boundarySDF = ConstantScalarField3(
-                           std::numeric_limits<double>::max()),
-                       const ScalarField3& fluidSDF = ConstantScalarField3(
-                           -std::numeric_limits<double>::max())) = 0;
+    virtual void Solve(
+        const CollocatedVectorGrid3& source, double diffusionCoefficient,
+        double timeIntervalInSeconds, CollocatedVectorGrid3* dest,
+        const ScalarField3& boundarySDF =
+            ConstantScalarField3{ std::numeric_limits<double>::max() },
+        const ScalarField3& fluidSDF = ConstantScalarField3{
+            -std::numeric_limits<double>::max() }) = 0;
 
     //!
     //! Solves diffusion equation for a face-centered vector field.
@@ -85,10 +97,11 @@ class GridDiffusionSolver3
     virtual void Solve(const FaceCenteredGrid3& source,
                        double diffusionCoefficient,
                        double timeIntervalInSeconds, FaceCenteredGrid3* dest,
-                       const ScalarField3& boundarySDF = ConstantScalarField3(
-                           std::numeric_limits<double>::max()),
-                       const ScalarField3& fluidSDF = ConstantScalarField3(
-                           -std::numeric_limits<double>::max())) = 0;
+                       const ScalarField3& boundarySDF =
+                           ConstantScalarField3{
+                               std::numeric_limits<double>::max() },
+                       const ScalarField3& fluidSDF = ConstantScalarField3{
+                           -std::numeric_limits<double>::max() }) = 0;
 };
 
 //! Shared pointer type for the GridDiffusionSolver3.
