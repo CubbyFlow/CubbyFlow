8f1f95a9e73687209a622d7a4991d5df1c82065e
Chris Ohk <utilForever@gmail.com>
2020-10-28 08:08:03 +0900

    fix: Add command to disable warning '-Wrange-loop-analysis'

--- Builds/CMake/CompileOptions.cmake
+++ Builds/CMake/CompileOptions.cmake
@@ -115,8 +115,9 @@ endif()
 if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
         -Wall
-        -Wno-register			# -> disable warning: ISO c++1z does not allow 'register' storage class specifier [-wregister] (caused by pybind11/python2.7)
-        -Wno-error=register		# -> disable warning: ISO c++1z does not allow 'register' storage class specifier [-wregister] (caused by pybind11/python2.7)
+        -Wno-register			 # -> disable warning: ISO c++1z does not allow 'register' storage class specifier [-wregister] (caused by pybind11/python2.7)
+        -Wno-error=register		 # -> disable warning: ISO c++1z does not allow 'register' storage class specifier [-wregister] (caused by pybind11/python2.7)
+        -Wno-range-loop-analysis # -> disable warning: loop variable 'kv' is always a copy because the range of type 'pybind11::dict' does not return a reference (caused by pybind11) 
         ${WARN_AS_ERROR_FLAGS}
         -std=c++1z
     )
