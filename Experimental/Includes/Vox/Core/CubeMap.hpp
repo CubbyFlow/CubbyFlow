883e4a7ad3a1ab5056f78d5f959243ea77193eee
snowapril <sinjihng@naver.com>
2021-01-01 11:51:46 +0000

    feat : add skeleton code for cubemap

---
+++ Experimental/Includes/Vox/Core/CubeMap.hpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+> File Name: CubeMap.hpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: Cube map class for image-based lighting
+> Created Time: 2020/12/30
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#ifndef CUBBYFLOW_VOX_CUBEMAP_HPP
+#define CUBBYFLOW_VOX_CUBEMAP_HPP
+
+#include <Vox/Core/Emitter.hpp>
+
+namespace Vox
+{
+    /**
+     * Cube map class for image-based lighting
+     */
+    class CubeMap : public Emitter
+    {
+     public:
+        CubeMap() = default;
+     protected:
+     private:
+    };
+
+};  // namespace Vox
+
+#endif
---
+++ Experimental/Sources/Vox/Core/CubeMap.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: CubeMap.cpp
+> Project Name: CubbyFlow
+> This code is based on Jet Framework that was created by Doyub Kim.
+> References: https://github.com/doyubkim/fluid-engine-dev
+> Purpose: Cube map class for image-based lighting
+> Created Time: 2020/12/30
+> Copyright (c) 2020, Ji-Hong snowapril
+*************************************************************************/
+#include <Vox/Core/CubeMap.hpp>
+
+namespace Vox
+{
+    
+    
+    
+};  // namespace Vox
