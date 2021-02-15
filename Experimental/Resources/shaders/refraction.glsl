9c2e784fb308b28cdfe2b37819e69d49b813297f
Snowapril <sinjihng@naver.com>
2021-02-15 19:05:00 +0900

    feat : Add environment mapping refraction shader code

---
+++ Experimental/Resources/shaders/refraction.glsl
@@ -0,0 +1,29 @@
+#version 330 core
+
+#include common/globals.glsl
+#include common/uniforms.glsl
+#include common/brdf.glsl
+
+precision highp float;
+precision highp int;
+precision highp sampler2D;
+precision highp samplerCube;
+precision highp isampler2D;
+precision highp sampler2DArray;
+
+in VSOUT {
+    vec3 normal;
+    vec3 worldPos;
+} fs_in;
+
+uniform samplerCube cubeMap;
+
+out vec4 fragColor;
+
+void main() {
+    float ratio = 1.0f / material.refractionRatio;
+
+    vec3 I = normalize(fs_in.worldPos - camera.position);
+    vec3 R = refract(I, normalize(fs_in.normal), ratio);
+    fragColor = vec4(texture(cubeMap, R).rgb, 1.0);
+}
