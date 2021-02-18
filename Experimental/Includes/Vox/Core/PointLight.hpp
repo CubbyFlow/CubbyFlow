825b70ca10a1420f0a674b5522cdde95d40ebf02
Snowapril <sinjihng@naver.com>
2021-02-18 15:44:19 +0900

    feat : make pointlight class as all point lights manager class

--- Experimental/Includes/Vox/Core/PointLight.hpp
+++ Experimental/Includes/Vox/Core/PointLight.hpp
@@ -10,34 +10,29 @@
 #ifndef CUBBYFLOW_VOX_POINT_LIGHT_HPP
 #define CUBBYFLOW_VOX_POINT_LIGHT_HPP
 
-#include <Vox/Core/Emitter.hpp>
+#include <Vox/Scene/VoxSceneObject.hpp>
 #include <Core/Vector/Vector3.hpp>
+#include <Core/Array/Array1.hpp>
 
 namespace Vox
 {
+    class Program;
+
    /**
     * Point light class
     */
-   class PointLight : public Emitter
+   class PointLight : public VoxSceneObject
    {
     public:
        PointLight() = default;
         
-      inline void SetPosition(const CubbyFlow::Vector3F& position)
-      {
-         _position = position;
-      }
-      
-      inline void SetColor(const CubbyFlow::Vector3F& color)
-      {
-         _color = color;
-      }
+       void AddLight(const CubbyFlow::Vector3F& position, const CubbyFlow::Vector3F& color);
 
-    protected:
-      void OnUploadProgram(ShaderParameters& params) override;
+       void UploadToProgram(const std::shared_ptr<Program>& program);
     private:
-      CubbyFlow::Vector3F _position;
-      CubbyFlow::Vector3F _color;
+      CubbyFlow::Array1<CubbyFlow::Vector3F> _positions;
+      CubbyFlow::Array1<CubbyFlow::Vector3F> _colors;
+      int _numLights { 0 };
    };
 
 };  // namespace Vox
--- Experimental/Sources/Vox/Core/PointLight.cpp
+++ Experimental/Sources/Vox/Core/PointLight.cpp
@@ -8,13 +8,26 @@
 > Copyright (c) 2020, Ji-Hong snowapril
 *************************************************************************/
 #include <Vox/Core/PointLight.hpp>
+#include <Vox/Core/Program.hpp>
 
 namespace Vox
 {
-   void PointLight::OnUploadProgram(ShaderParameters& params)
-   {
-      params.SetParameter("light.position", _position);
-      params.SetParameter("light.color", _color);
-   }
+	void PointLight::AddLight(const CubbyFlow::Vector3F& position, const CubbyFlow::Vector3F& color)
+	{
+		_positions.Append(position);
+		_colors.Append(color);
+		++_numLights;
+	}
+
+	void PointLight::UploadToProgram(const std::shared_ptr<Program>& program)
+	{
+		auto& params = program->GetParameters();
+		params.SetParameter("numLights", _numLights);
+		for (int i = 0; i < _numLights; ++i)
+		{
+			params.SetParameter("light[" + std::to_string(i) +"].position", _positions[i]);
+			params.SetParameter("light[" + std::to_string(i) +"].color", _colors[i]);
+		}
+	}
 
 };  // namespace Vox
