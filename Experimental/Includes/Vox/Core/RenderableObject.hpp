f12644b06266477b2fb726aec5b5fe93f4a30392
Snowapril <sinjihng@naver.com>
2021-02-20 12:26:05 +0900

    feat(refactor) : add comments & [[nodiscard]] attirubtes

--- Experimental/Includes/Vox/Core/RenderableObject.hpp
+++ Experimental/Includes/Vox/Core/RenderableObject.hpp
@@ -3,7 +3,7 @@
 > Project Name: CubbyFlow
 > This code is based on Jet Framework that was created by Doyub Kim.
 > References: https://github.com/doyubkim/fluid-engine-dev
-> Purpose: Static Object wrapper class.
+> Purpose: Renderable object wrapper which will be rendered in main frame.
 > Created Time: 2020/08/18
 > Copyright (c) 2020, Ji-Hong snowapril
 *************************************************************************/
@@ -14,7 +14,6 @@
 #include <Core/Geometry/BoundingBox3.hpp>
 #include <Core/Matrix/Matrix4x4.hpp>
 #include <Core/Array/Array1.hpp>
-#include <string>
 #include <memory>
 
 namespace Vox {
@@ -30,33 +29,32 @@ namespace Vox {
         RenderableObject();
         //! Default Destructor.
         virtual ~RenderableObject();
-        
+        //! Returns the geometry mesh of the object with given index.
+        [[nodiscard]] const std::shared_ptr<Mesh> GetGeometryMesh(size_t index) const;
+        //! Returns the number of the contained geometry mesh.
+        [[nodiscard]] size_t GetNumberOfGeometryMesh() const;
+        //! Returns the material of the object.
+        [[nodiscard]] const std::shared_ptr<Material> GetMaterial() const;
+        //! Returns the bounding box of this renderable object.
+        [[nodiscard]] CubbyFlow::BoundingBox3F GetBoundingBox() const;
         //! Set model matrix
         void SetModelMatrix(const CubbyFlow::Matrix4x4F& model);
         //! Append Geometry mesh instance to the array..
         void AddGeometryMesh(const std::shared_ptr<Mesh> mesh);
-        //! Returns the geometry mesh of the object with given index.
-        const std::shared_ptr<Mesh> GetGeometryMesh(size_t index) const;
         //! Clear the geometry mesh list
         void ClearGeometryMesh();
-        //! Returns the number of the contained geometry mesh.
-        size_t GetNumberOfGeometryMesh() const;
         //! Set the material of the renderable object.
         void AttachMaterial(std::shared_ptr<Material> material);
-        //! Returns the material of the object.
-        const std::shared_ptr<Material> GetMaterial() const;
         //! Draw this renderable object with configured settings.
-        virtual void DrawRenderableObject(const std::shared_ptr<FrameContext>& ctx) {(void)ctx;};
-        //! Returns the bounding box of this renderable object.
-        CubbyFlow::BoundingBox3F GetBoundingBox() const;
+        virtual void DrawRenderableObject(const std::shared_ptr<FrameContext>& ctx) { (void)ctx; };
     protected:
+        //! Configure render settings, such as material bounding, buffer binding and etc..
         virtual void ConfigureRenderSettings(const std::shared_ptr<FrameContext>& ctx) {(void)ctx;};
         
-        CubbyFlow::Matrix4x4F _modelMatrix;
-        CubbyFlow::BoundingBox3F _boundingBox;
-        CubbyFlow::Array1<std::shared_ptr<Mesh>> _meshes;
-        std::shared_ptr<Material> _material;
-    private:
+        CubbyFlow::Matrix4x4F _modelMatrix; // local-to-world matrix which will be calculated in shader
+        CubbyFlow::BoundingBox3F _boundingBox; // bounding box of the whole meshes
+        CubbyFlow::Array1<std::shared_ptr<Mesh>> _meshes; // whole meshes of this renderable object
+        std::shared_ptr<Material> _material; // material of this renderable object
     };
 }
 
