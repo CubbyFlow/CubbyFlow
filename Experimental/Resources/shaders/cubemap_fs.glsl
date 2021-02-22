ba851b2f8df253538e6d75e05796a84f4280127a
Snowapril <sinjihng@naver.com>
2021-02-22 15:18:03 +0900

    feat : remove gamma corret & tone mapping as they already done in later

--- Experimental/Resources/shaders/cubemap_fs.glsl
+++ Experimental/Resources/shaders/cubemap_fs.glsl
@@ -20,10 +20,5 @@ out vec4 fragColor;
 
 void main()
 {
-	vec3 envColor = texture(cubeMap, fs_in.localPos).rgb;
-
-	envColor = envColor / (envColor + vec3(1.0));
-	envColor = pow(envColor, vec3(1.0 / 2.2));
-
-	fragColor = vec4(envColor, 1.0);
+	fragColor = vec4(texture(cubeMap, fs_in.localPos).rgb, 1.0);
 }
