c2cfffdb4a275ab78f06efd4f5383f3c612b1737
Chris Ohk <utilforever@gmail.com>
2021-03-07 21:07:14 +0900

    fix: Correct minor typo

--- Includes/Core/Utils/Serialization-Impl.hpp
+++ Includes/Core/Utils/Serialization-Impl.hpp
@@ -18,7 +18,7 @@ namespace CubbyFlow
 template <typename T>
 void Serialize(const ConstArrayView1<T>& array, std::vector<uint8_t>* buffer)
 {
-    const size_t size = sizeof(T) * array.size();
+    const size_t size = sizeof(T) * array.Length();
     Serialize(reinterpret_cast<const uint8_t*>(array.data()), size, buffer);
 }
 
