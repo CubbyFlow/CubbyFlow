1aeaf8a5081c2b1efbb9c03cee0242fd1c3f4f05
Chris Ohk <utilforever@gmail.com>
2021-03-18 23:53:42 +0900

    refactor: Create class 'Collider'

---
+++ Includes/Core/Geometry/Collider.hpp
@@ -0,0 +1,134 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_COLLIDER_HPP
+#define CUBBYFLOW_COLLIDER_HPP
+
+#include <Core/Geometry/Surface.hpp>
+
+#include <functional>
+
+namespace CubbyFlow
+{
+//!
+//! \brief Abstract base class for generic collider object.
+//!
+//! This class contains basic interfaces for colliders. Most of the
+//! functionalities are implemented within this class, except the member
+//! function Collider::VelocityAt. This class also let the subclasses to
+//! provide a Surface instance to define collider surface using
+//! Collider::SetSurface function.
+//!
+template <size_t N>
+class Collider
+{
+ public:
+    //!
+    //! \brief Callback function type for update calls.
+    //!
+    //! This type of callback function will take the collider pointer, current
+    //! time, and time interval in seconds.
+    //!
+    using OnBeginUpdateCallback =
+        std::function<void(Collider*, double, double)>;
+
+    //! Default constructor.
+    Collider() = default;
+
+    //! Default destructor.
+    virtual ~Collider() = default;
+
+    //! Returns the velocity of the collider at given \p point.
+    virtual Vector<double, N> VelocityAt(
+        const Vector<double, N>& point) const = 0;
+
+    //!
+    //! Resolves collision for given point.
+    //!
+    //! \param radius Radius of the colliding point.
+    //! \param restitutionCoefficient Defines the restitution effect.
+    //! \param position Input and output position of the point.
+    //! \param velocity Input and output velocity of the point.
+    //!
+    void ResolveCollision(double radius, double restitutionCoefficient,
+                          Vector<double, N>* position,
+                          Vector<double, N>* velocity);
+
+    //! Returns friction coefficent.
+    double GetFrictionCoefficient() const;
+
+    //!
+    //! \brief Sets the friction coefficient.
+    //!
+    //! This function assigns the friction coefficient to the collider. Any
+    //! negative inputs will be clamped to zero.
+    //!
+    void SetFrictionCoefficient(double newFrictionCoefficient);
+
+    //! Returns the surface instance.
+    const std::shared_ptr<Surface<N>>& GetSurface() const;
+
+    //! Updates the collider state.
+    void Update(double currentTimeInSeconds, double timeIntervalInSeconds);
+
+    //!
+    //! \brief      Sets the callback function to be called when
+    //!             Collider::update function is invoked.
+    //!
+    //! The callback function takes current simulation time in seconds unit. Use
+    //! this callback to track any motion or state changes related to this
+    //! collider.
+    //!
+    //! \param[in]  callback The callback function.
+    //!
+    void SetOnBeginUpdateCallback(const OnBeginUpdateCallback& callback);
+
+ protected:
+    //! Internal query result structure.
+    struct ColliderQueryResult final
+    {
+        double distance = 0.0;
+        Vector<double, N> point;
+        Vector<double, N> normal;
+        Vector<double, N> velocity;
+    };
+
+    //! Assigns the surface instance from the subclass.
+    void SetSurface(const std::shared_ptr<Surface<N>>& newSurface);
+
+    //! Outputs closest point's information.
+    void GetClosestPoint(const std::shared_ptr<Surface<N>>& surface,
+                         const Vector<double, N>& queryPoint,
+                         ColliderQueryResult* result) const;
+
+    //! Returns true if given point is in the opposite side of the surface.
+    bool IsPenetrating(const ColliderQueryResult& colliderPoint,
+                       const Vector<double, N>& position, double radius);
+
+ private:
+    std::shared_ptr<Surface<N>> m_surface;
+    double m_frictionCoeffient = 0.0;
+    OnBeginUpdateCallback m_onUpdateCallback;
+};
+
+//! 2-D collider type.
+using Collider2 = Collider<2>;
+
+//! 3-D collider type.
+using Collider3 = Collider<3>;
+
+//! Shared pointer type for the Collider2.
+using Collider2Ptr = std::shared_ptr<Collider2>;
+
+//! Shared pointer type for the Collider3.
+using Collider3Ptr = std::shared_ptr<Collider3>;
+}  // namespace CubbyFlow
+
+#endif
---
+++ Sources/Core/Geometry/Collider.cpp
@@ -0,0 +1,158 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Core/Geometry/Collider.hpp>
+
+#include <cassert>
+
+namespace CubbyFlow
+{
+template <size_t N>
+void Collider<N>::ResolveCollision(double radius, double restitutionCoefficient,
+                                   Vector<double, N>* newPosition,
+                                   Vector<double, N>* newVelocity)
+{
+    assert(m_surface);
+
+    if (!m_surface->IsValidGeometry())
+    {
+        return;
+    }
+
+    ColliderQueryResult colliderPoint;
+
+    GetClosestPoint(m_surface, *newPosition, &colliderPoint);
+
+    // Check if the new position is penetrating the surface
+    if (IsPenetrating(colliderPoint, *newPosition, radius))
+    {
+        // Target point is the closest non-penetrating position from the
+        // new position.
+        Vector<double, N> targetNormal = colliderPoint.normal;
+        Vector<double, N> targetPoint =
+            colliderPoint.point + radius * targetNormal;
+        Vector<double, N> colliderVelAtTargetPoint = colliderPoint.velocity;
+
+        // Get new candidate relative velocity from the target point.
+        Vector<double, N> relativeVel = *newVelocity - colliderVelAtTargetPoint;
+        double normalDotRelativeVel = targetNormal.Dot(relativeVel);
+        Vector<double, N> relativeVelN = normalDotRelativeVel * targetNormal;
+        Vector<double, N> relativeVelT = relativeVel - relativeVelN;
+
+        // Check if the velocity is facing opposite direction of the surface
+        // normal
+        if (normalDotRelativeVel < 0.0)
+        {
+            // Apply restitution coefficient to the surface normal component of
+            // the velocity
+            Vector<double, N> deltaRelativeVelN =
+                (-restitutionCoefficient - 1.0) * relativeVelN;
+            relativeVelN *= -restitutionCoefficient;
+
+            // Apply friction to the tangential component of the velocity
+            // From Bridson et al., Robust Treatment of Collisions, Contact and
+            // Friction for Cloth Animation, 2002
+            // http://graphics.stanford.edu/papers/cloth-sig02/cloth.pdf
+            if (relativeVelT.LengthSquared() > 0.0)
+            {
+                double frictionScale = std::max(
+                    1.0 - m_frictionCoeffient * deltaRelativeVelN.Length() /
+                              relativeVelT.Length(),
+                    0.0);
+                relativeVelT *= frictionScale;
+            }
+
+            // Reassemble the components
+            *newVelocity =
+                relativeVelN + relativeVelT + colliderVelAtTargetPoint;
+        }
+
+        // Geometric fix
+        *newPosition = targetPoint;
+    }
+}
+
+template <size_t N>
+double Collider<N>::GetFrictionCoefficient() const
+{
+    return m_frictionCoeffient;
+}
+
+template <size_t N>
+void Collider<N>::SetFrictionCoefficient(double newFrictionCoefficient)
+{
+    m_frictionCoeffient = std::max(newFrictionCoefficient, 0.0);
+}
+
+template <size_t N>
+const std::shared_ptr<Surface<N>>& Collider<N>::GetSurface() const
+{
+    return m_surface;
+}
+
+template <size_t N>
+void Collider<N>::SetSurface(const std::shared_ptr<Surface<N>>& newSurface)
+{
+    m_surface = newSurface;
+}
+
+template <size_t N>
+void Collider<N>::GetClosestPoint(const std::shared_ptr<Surface<N>>& surface,
+                                  const Vector<double, N>& queryPoint,
+                                  ColliderQueryResult* result) const
+{
+    result->distance = surface->ClosestDistance(queryPoint);
+    result->point = surface->ClosestPoint(queryPoint);
+    result->normal = surface->ClosestNormal(queryPoint);
+    result->velocity = VelocityAt(queryPoint);
+}
+
+template <size_t N>
+bool Collider<N>::IsPenetrating(const ColliderQueryResult& colliderPoint,
+                                const Vector<double, N>& position,
+                                double radius)
+{
+    // If the new candidate position of the particle is on the other side of
+    // the surface OR the new distance to the surface is less than the
+    // particle's radius, this particle is in colliding state.
+    return (position - colliderPoint.point).Dot(colliderPoint.normal) < 0.0 ||
+           colliderPoint.distance < radius;
+}
+
+template <size_t N>
+void Collider<N>::Update(double currentTimeInSeconds,
+                         double timeIntervalInSeconds)
+{
+    assert(m_surface);
+
+    if (!m_surface->IsValidGeometry())
+    {
+        return;
+    }
+
+    m_surface->UpdateQueryEngine();
+
+    if (m_onUpdateCallback)
+    {
+        m_onUpdateCallback(this, currentTimeInSeconds, timeIntervalInSeconds);
+    }
+}
+
+template <size_t N>
+void Collider<N>::SetOnBeginUpdateCallback(
+    const OnBeginUpdateCallback& callback)
+{
+    m_onUpdateCallback = callback;
+}
+
+template class Collider<2>;
+
+template class Collider<3>;
+}  // namespace CubbyFlow
