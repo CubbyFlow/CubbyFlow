76f8a3fc68ceef3ece292d1dfd29ab8b45b0bce2
Chris Ohk <utilforever@gmail.com>
2021-03-20 01:06:33 +0900

    refactor: Create class 'Plane'

---
+++ Includes/Core/Geometry/Plane.hpp
@@ -0,0 +1,112 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_PLANE_HPP
+#define CUBBYFLOW_PLANE_HPP
+
+#include <Core/Geometry/Surface.hpp>
+
+namespace CubbyFlow
+{
+//!
+//! \brief N-D plane geometry.
+//!
+//! This class represents N-D plane geometry which extends Surface by
+//! overriding surface-related queries.
+//!
+template <size_t N>
+class Plane final : public Surface<N>
+{
+ public:
+    class Builder;
+
+    //! Constructs a plane that crosses (0, 0, ...) with surface normal
+    //! (y-axis).
+    Plane(const Transform<N>& _transform = Transform<N>{},
+          bool _isNormalFlipped = false);
+
+    //! Constructs a plane that cross \p point with surface normal \p normal.
+    Plane(const Vector<double, N>& normal, const Vector<double, N>& point,
+          const Transform<N>& _transform = Transform<N>{},
+          bool _isNormalFlipped = false);
+
+    //! Copy constructor.
+    Plane(const Plane& other);
+
+    //! Returns true if bounding box can be defined.
+    bool IsBounded() const override;
+
+    //! Returns builder fox Plane.
+    static Builder GetBuilder();
+
+    //! Plane normal.
+    Vector<double, N> normal = Vector<double, N>::MakeUnitY();
+
+    //! Point that lies on the plane.
+    Vector<double, N> point;
+
+ private:
+    Vector<double, N> ClosestPointLocal(
+        const Vector<double, N>& otherPoint) const override;
+
+    bool IntersectsLocal(const Ray<double, N>& ray) const override;
+
+    BoundingBox<double, N> BoundingBoxLocal() const override;
+
+    Vector<double, N> ClosestNormalLocal(
+        const Vector<double, N>& otherPoint) const override;
+
+    SurfaceRayIntersection<N> ClosestIntersectionLocal(
+        const Ray<double, N>& ray) const override;
+};
+
+//! 2-D plane type.
+using Plane2 = Plane<2>;
+
+//! 3-D plane type.
+using Plane3 = Plane<3>;
+
+//! Shared pointer for the Plane2 type.
+using Plane2Ptr = std::shared_ptr<Plane2>;
+
+//! Shared pointer for the Plane3 type.
+using Plane3Ptr = std::shared_ptr<Plane3>;
+
+//!
+//! \brief Front-end to create Plane objects step by step.
+//!
+template <size_t N>
+class Plane<N>::Builder final
+    : public SurfaceBuilderBase<N, typename Plane<N>::Builder>
+{
+ public:
+    //! Returns builder with plane normal.
+    Builder& WithNormal(const Vector<double, N>& _normal);
+
+    //! Returns builder with point on the plane.
+    Builder& WithPoint(const Vector<double, N>& _point);
+
+    //! Builds Plane.
+    Plane Build() const;
+
+    //! Builds shared pointer of Plane instance.
+    std::shared_ptr<Plane<N>> MakeShared() const;
+
+ private:
+    using Base = SurfaceBuilderBase<N, typename Plane<N>::Builder>;
+    using Base::m_isNormalFlipped;
+    using Base::m_transform;
+
+    Vector<double, N> m_normal = Vector<double, N>::MakeUnitY();
+    Vector<double, N> m_point;
+};
+}  // namespace CubbyFlow
+
+#endif
---
+++ Sources/Core/Geometry/Plane.cpp
@@ -0,0 +1,158 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Core/Geometry/Plane.hpp>
+
+#include <limits>
+
+namespace CubbyFlow
+{
+template <size_t N>
+Plane<N>::Plane(const Transform<N>& _transform, bool _isNormalFlipped)
+    : Surface<N>{ _transform, _isNormalFlipped }
+{
+    // Do nothing
+}
+
+template <size_t N>
+Plane<N>::Plane(const Vector<double, N>& _normal,
+                const Vector<double, N>& _point, const Transform<N>& _transform,
+                bool _isNormalFlipped)
+    : Surface<N>{ _transform, _isNormalFlipped }, normal(_normal), point(_point)
+{
+    // Do nothing
+}
+
+template <size_t N>
+Plane<N>::Plane(const Plane& other)
+    : Surface<N>{ other }, normal(other.normal), point(other.point)
+{
+    // Do nothing
+}
+
+template <size_t N>
+bool Plane<N>::IsBounded() const
+{
+    return false;
+}
+
+template <size_t N>
+Vector<double, N> Plane<N>::ClosestPointLocal(
+    const Vector<double, N>& otherPoint) const
+{
+    Vector<double, N> r = otherPoint - point;
+    return r - normal.Dot(r) * normal + point;
+}
+
+template <size_t N>
+Vector<double, N> Plane<N>::ClosestNormalLocal(
+    const Vector<double, N>& otherPoint) const
+{
+    UNUSED_VARIABLE(otherPoint);
+    return normal;
+}
+
+template <size_t N>
+bool Plane<N>::IntersectsLocal(const Ray<double, N>& ray) const
+{
+    return std::fabs(ray.direction.Dot(normal)) > 0;
+}
+
+template <size_t N>
+SurfaceRayIntersection<N> Plane<N>::ClosestIntersectionLocal(
+    const Ray<double, N>& ray) const
+{
+    SurfaceRayIntersection<N> intersection;
+    double dDotN = ray.direction.Dot(normal);
+
+    // Check if not parallel
+    if (std::fabs(dDotN) > 0)
+    {
+        double t = normal.Dot(point - ray.origin) / dDotN;
+
+        if (t >= 0.0)
+        {
+            intersection.isIntersecting = true;
+            intersection.distance = t;
+            intersection.point = ray.PointAt(t);
+            intersection.normal = normal;
+        }
+    }
+
+    return intersection;
+}
+
+template <size_t N>
+BoundingBox<double, N> Plane<N>::BoundingBoxLocal() const
+{
+    Vector<double, N> maxCorner =
+        Vector<double, N>::MakeConstant(std::numeric_limits<double>::max());
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        Vector<double, N> n;
+        Vector<double, N> corner = maxCorner;
+
+        n[i] = 1.0;
+        corner[i] = 0.0;
+
+        // See if the plane is axis-aligned and return flat box if true.
+        if (std::fabs(normal.Dot(n) - 1.0) <
+            std::numeric_limits<double>::epsilon())
+        {
+            return BoundingBox<double, N>{ point - corner, point + corner };
+        }
+    }
+
+    // Otherwise, the plane does not have bbox.
+    return BoundingBox<double, N>{ -maxCorner, maxCorner };
+}
+
+template <size_t N>
+typename Plane<N>::Builder Plane<N>::GetBuilder()
+{
+    return Builder{};
+}
+
+template <size_t N>
+typename Plane<N>::Builder& Plane<N>::Builder::WithNormal(
+    const Vector<double, N>& _normal)
+{
+    m_normal = _normal;
+    return *this;
+}
+
+template <size_t N>
+typename Plane<N>::Builder& Plane<N>::Builder::WithPoint(
+    const Vector<double, N>& _point)
+{
+    m_point = _point;
+    return *this;
+}
+
+template <size_t N>
+Plane<N> Plane<N>::Builder::Build() const
+{
+    return Plane<N>(m_normal, m_point, m_transform, m_isNormalFlipped);
+}
+
+template <size_t N>
+std::shared_ptr<Plane<N>> Plane<N>::Builder::MakeShared() const
+{
+    return std::shared_ptr<Plane<N>>{
+        new Plane<N>(m_normal, m_point, m_transform, m_isNormalFlipped),
+        [](Plane<N>* obj) { delete obj; }
+    };
+}
+
+template class Plane<2>;
+
+template class Plane<3>;
+}  // namespace CubbyFlow
