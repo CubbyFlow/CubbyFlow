5322261191221f84e461bb84baa2e1cb383af20e
Chris Ohk <utilforever@gmail.com>
2021-03-22 21:22:32 +0900

    fix: Delete code to check template parameter 'T' is floating type

--- Includes/Core/Geometry/BoundingBox.hpp
+++ Includes/Core/Geometry/BoundingBox.hpp
@@ -47,9 +47,6 @@ class BoundingBox
 {
  public:
     static_assert(N > 0, "Dimension should be greater than 0");
-    static_assert(
-        std::is_floating_point<T>::value,
-        "BoundingBox only can be instantiated with floating point types");
 
     using VectorType = Vector<T, N>;
     using RayType = Ray<T, N>;
