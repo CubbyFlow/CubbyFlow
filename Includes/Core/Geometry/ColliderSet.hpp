38573b66ffb5ebc1efca750b3a4bd90b0c13870e
Chris Ohk <utilforever@gmail.com>
2021-03-24 00:09:49 +0900

    refactor: Create class 'ColliderSet'

---
+++ Includes/Core/Geometry/ColliderSet.hpp
@@ -0,0 +1,92 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_COLLIDER_SET_HPP
+#define CUBBYFLOW_COLLIDER_SET_HPP
+
+#include <Core/Array/Array.hpp>
+#include <Core/Geometry/Collider.hpp>
+#include <Core/Geometry/SurfaceSet.hpp>
+
+#include <memory>
+
+namespace CubbyFlow
+{
+//! Collection of N-D colliders
+template <size_t N>
+class ColliderSet final : public Collider<N>
+{
+ public:
+    class Builder;
+
+    using Collider<N>::GetSurface;
+    using Collider<N>::SetSurface;
+
+    //! Default constructor.
+    ColliderSet();
+
+    //! Constructs with other colliders.
+    explicit ColliderSet(
+        const ConstArrayView1<std::shared_ptr<Collider<N>>>& others);
+
+    //! Returns the velocity of the collider at given \p point.
+    Vector<double, N> VelocityAt(const Vector<double, N>& point) const override;
+
+    //! Adds a collider to the set.
+    void AddCollider(const std::shared_ptr<Collider<N>>& collider);
+
+    //! Returns number of colliders.
+    size_t NumberOfColliders() const;
+
+    //! Returns collider at index \p i.
+    std::shared_ptr<Collider<N>> GetCollider(size_t i) const;
+
+    //! Returns builder for ColliderSet.
+    static Builder GetBuilder();
+
+ private:
+    Array1<std::shared_ptr<Collider<N>>> m_colliders;
+};
+
+//! 2-D ColliderSet type.
+using ColliderSet2 = ColliderSet<2>;
+
+//! 3-D ColliderSet type.
+using ColliderSet3 = ColliderSet<3>;
+
+//! Shared pointer for the ColliderSet2 type.
+using ColliderSet2Ptr = std::shared_ptr<ColliderSet2>;
+
+//! Shared pointer for the ColliderSet3 type.
+using ColliderSet3Ptr = std::shared_ptr<ColliderSet3>;
+
+//!
+//! \brief Front-end to create ColliderSet objects step by step.
+//!
+template <size_t N>
+class ColliderSet<N>::Builder final
+{
+ public:
+    //! Returns builder with other colliders.
+    Builder& WithColliders(
+        const ConstArrayView1<std::shared_ptr<Collider<N>>>& others);
+
+    //! Builds ColliderSet.
+    ColliderSet Build() const;
+
+    //! Builds shared pointer of ColliderSet instance.
+    std::shared_ptr<ColliderSet<N>> MakeShared() const;
+
+ private:
+    Array1<std::shared_ptr<Collider<N>>> m_colliders;
+};
+}  // namespace CubbyFlow
+
+#endif
---
+++ Sources/Core/Geometry/ColliderSet.cpp
@@ -0,0 +1,115 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Core/Geometry/ColliderSet.hpp>
+#include <Core/Geometry/SurfaceSet.hpp>
+
+#include <limits>
+
+namespace CubbyFlow
+{
+template <size_t N>
+ColliderSet<N>::ColliderSet()
+    : ColliderSet{ Array1<std::shared_ptr<Collider<N>>>{} }
+{
+    // Do nothing
+}
+
+template <size_t N>
+ColliderSet<N>::ColliderSet(
+    const ConstArrayView1<std::shared_ptr<Collider<N>>>& others)
+{
+    SetSurface(std::make_shared<SurfaceSet<N>>());
+
+    for (const auto& collider : others)
+    {
+        AddCollider(collider);
+    }
+}
+
+template <size_t N>
+Vector<double, N> ColliderSet<N>::VelocityAt(
+    const Vector<double, N>& point) const
+{
+    size_t closestCollider = std::numeric_limits<size_t>::max();
+    double closestDist = std::numeric_limits<double>::max();
+
+    for (size_t i = 0; i < m_colliders.Length(); ++i)
+    {
+        const double dist =
+            m_colliders[i]->GetSurface()->ClosestDistance(point);
+
+        if (dist < closestDist)
+        {
+            closestDist = dist;
+            closestCollider = i;
+        }
+    }
+
+    if (closestCollider != std::numeric_limits<size_t>::max())
+    {
+        return m_colliders[closestCollider]->VelocityAt(point);
+    }
+
+    return Vector<double, N>{};
+}
+
+template <size_t N>
+void ColliderSet<N>::AddCollider(const std::shared_ptr<Collider<N>>& collider)
+{
+    std::shared_ptr<SurfaceSet<N>> surfaceSet =
+        std::dynamic_pointer_cast<SurfaceSet<N>>(GetSurface());
+    m_colliders.Append(collider);
+    surfaceSet->AddSurface(collider->GetSurface());
+}
+
+template <size_t N>
+size_t ColliderSet<N>::NumberOfColliders() const
+{
+    return m_colliders.Length();
+}
+
+template <size_t N>
+std::shared_ptr<Collider<N>> ColliderSet<N>::GetCollider(size_t i) const
+{
+    return m_colliders[i];
+}
+
+template <size_t N>
+typename ColliderSet<N>::Builder ColliderSet<N>::GetBuilder()
+{
+    return Builder{};
+}
+
+template <size_t N>
+typename ColliderSet<N>::Builder& ColliderSet<N>::Builder::WithColliders(
+    const ConstArrayView1<std::shared_ptr<Collider<N>>>& others)
+{
+    m_colliders = others;
+    return *this;
+}
+
+template <size_t N>
+ColliderSet<N> ColliderSet<N>::Builder::Build() const
+{
+    return ColliderSet{ m_colliders };
+}
+
+template <size_t N>
+std::shared_ptr<ColliderSet<N>> ColliderSet<N>::Builder::MakeShared() const
+{
+    return std::shared_ptr<ColliderSet>(new ColliderSet{ m_colliders },
+                                        [](ColliderSet* obj) { delete obj; });
+}
+
+template class ColliderSet<2>;
+
+template class ColliderSet<3>;
+}  // namespace CubbyFlow
