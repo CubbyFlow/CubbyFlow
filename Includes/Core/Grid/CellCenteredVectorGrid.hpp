8281afa5e7c9c92126be65c3265e3d9e0b99a27f
Chris Ohk <utilforever@gmail.com>
2021-04-14 13:16:03 +0900

    refactor: Create class 'CellCenteredVectorGrid'

---
+++ Includes/Core/Grid/CellCenteredVectorGrid.hpp
@@ -0,0 +1,155 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_CELL_CENTERED_VECTOR_GRID_HPP
+#define CUBBYFLOW_CELL_CENTERED_VECTOR_GRID_HPP
+
+#include <Core/Grid/CollocatedVectorGrid.hpp>
+
+namespace CubbyFlow
+{
+//!
+//! \brief N-D Cell-centered vector grid structure.
+//!
+//! This class represents N-D cell-centered vector grid which extends
+//! CollocatedVectorGrid. As its name suggests, the class defines the data
+//! point at the center of a grid cell. Thus, the dimension of data points are
+//! equal to the dimension of the cells.
+//!
+template <size_t N>
+class CellCenteredVectorGrid final : public CollocatedVectorGrid<N>
+{
+ public:
+    CUBBYFLOW_GRID_TYPE_NAME(CellCenteredVectorGrid, 2)
+
+    class Builder;
+
+    using typename CollocatedVectorGrid<N>::DataPositionFunc;
+    using CollocatedVectorGrid<N>::Resize;
+    using CollocatedVectorGrid<N>::Resolution;
+    using CollocatedVectorGrid<N>::Origin;
+    using CollocatedVectorGrid<N>::GridSpacing;
+    using CollocatedVectorGrid<N>::DataView;
+    using CollocatedVectorGrid<N>::DataPosition;
+
+    //! Constructs zero-sized grid.
+    CellCenteredVectorGrid() = default;
+
+    //! Constructs a grid with given resolution, grid spacing, origin and
+    //! initial value.
+    CellCenteredVectorGrid(
+        const Vector<size_t, N>& resolution,
+        const Vector<double, N>& gridSpacing =
+            Vector<double, N>::MakeConstant(1.0),
+        const Vector<double, N>& origin = Vector<double, N>{},
+        const Vector<double, N>& initialValue = Vector<double, N>{});
+
+    //! Copy constructor.
+    CellCenteredVectorGrid(const CellCenteredVectorGrid& other);
+
+    //! Returns the actual data point size.
+    Vector<size_t, N> DataSize() const override;
+
+    //! Returns data position for the grid point at (0, 0).
+    //! Note that this is different from origin() since origin() returns
+    //! the lower corner point of the bounding box.
+    Vector<double, N> DataOrigin() const override;
+
+    //!
+    //! \brief Swaps the contents with the given \p other grid.
+    //!
+    //! This function swaps the contents of the grid instance with the given
+    //! grid object \p other only if \p other has the same type with this grid.
+    //!
+    void Swap(Grid<N>* other) override;
+
+    //! Sets the contents with the given \p other grid.
+    void Set(const CellCenteredVectorGrid& other);
+
+    //! Sets the contents with the given \p other grid.
+    CellCenteredVectorGrid& operator=(const CellCenteredVectorGrid& other);
+
+    //! Fills the grid with given value.
+    void Fill(const Vector<double, N>& value,
+              ExecutionPolicy policy = ExecutionPolicy::Parallel) override;
+
+    //! Fills the grid with given function.
+    void Fill(
+        const std::function<Vector<double, N>(const Vector<double, N>&)>& func,
+        ExecutionPolicy policy = ExecutionPolicy::Parallel) override;
+
+    //! Returns the copy of the grid instance.
+    std::shared_ptr<VectorGrid<N>> Clone() const override;
+
+    //! Returns builder fox CellCenteredVectorGrid.
+    static Builder GetBuilder();
+
+ protected:
+    using CollocatedVectorGrid<N>::SwapCollocatedVectorGrid;
+    using CollocatedVectorGrid<N>::SetCollocatedVectorGrid;
+};
+
+//! 2-D CellCenteredVectorGrid type.
+using CellCenteredVectorGrid2 = CellCenteredVectorGrid<2>;
+
+//! 3-D CellCenteredVectorGrid type.
+using CellCenteredVectorGrid3 = CellCenteredVectorGrid<3>;
+
+//! Shared pointer for the CellCenteredVectorGrid2 type.
+using CellCenteredVectorGrid2Ptr = std::shared_ptr<CellCenteredVectorGrid2>;
+
+//! Shared pointer for the CellCenteredVectorGrid3 type.
+using CellCenteredVectorGrid3Ptr = std::shared_ptr<CellCenteredVectorGrid3>;
+
+//!
+//! \brief Front-end to create CellCenteredVectorGrid objects step by step.
+//!
+template <size_t N>
+class CellCenteredVectorGrid<N>::Builder final : public VectorGridBuilder<N>
+{
+ public:
+    //! Returns builder with resolution.
+    Builder& WithResolution(const Vector<size_t, N>& resolution);
+
+    //! Returns builder with grid spacing.
+    Builder& WithGridSpacing(const Vector<double, N>& gridSpacing);
+
+    //! Returns builder with grid origin.
+    Builder& WithOrigin(const Vector<double, N>& gridOrigin);
+
+    //! Returns builder with initial value.
+    Builder& WithInitialValue(const Vector<double, N>& initialVal);
+
+    //! Builds CellCenteredVectorGrid instance.
+    CellCenteredVectorGrid Build() const;
+
+    //! Builds shared pointer of CellCenteredVectorGrid instance.
+    std::shared_ptr<CellCenteredVectorGrid> MakeShared() const;
+
+    //!
+    //! \brief Builds shared pointer of CellCenteredVectorGrid instance.
+    //!
+    //! This is an overriding function that implements VectorGridBuilder.
+    //!
+    std::shared_ptr<VectorGrid<N>> Build(
+        const Vector<size_t, N>& resolution,
+        const Vector<double, N>& gridSpacing,
+        const Vector<double, N>& gridOrigin,
+        const Vector<double, N>& initialVal) const override;
+
+ private:
+    Vector<size_t, N> m_resolution = Vector<size_t, N>::MakeConstant(1);
+    Vector<double, N> m_gridSpacing = Vector<double, N>::MakeConstant(1.0);
+    Vector<double, N> m_gridOrigin;
+    Vector<double, N> m_initialVal;
+};
+}  // namespace CubbyFlow
+
+#endif
---
+++ Sources/Core/Grid/CellCenteredVectorGrid.cpp
@@ -0,0 +1,177 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Core/Grid/CellCenteredVectorGrid.hpp>
+
+namespace CubbyFlow
+{
+template <size_t N>
+CellCenteredVectorGrid<N>::CellCenteredVectorGrid(
+    const Vector<size_t, N>& resolution, const Vector<double, N>& gridSpacing,
+    const Vector<double, N>& origin, const Vector<double, N>& initialValue)
+{
+    Resize(resolution, gridSpacing, origin, initialValue);
+}
+
+template <size_t N>
+CellCenteredVectorGrid<N>::CellCenteredVectorGrid(
+    const CellCenteredVectorGrid& other)
+{
+    Set(other);
+}
+
+template <size_t N>
+Vector<size_t, N> CellCenteredVectorGrid<N>::DataSize() const
+{
+    return Resolution();
+}
+
+template <size_t N>
+Vector<double, N> CellCenteredVectorGrid<N>::DataOrigin() const
+{
+    return Origin() + 0.5 * GridSpacing();
+}
+
+template <size_t N>
+void CellCenteredVectorGrid<N>::Swap(Grid<N>* other)
+{
+    if (auto sameType = dynamic_cast<CellCenteredVectorGrid*>(other); sameType)
+    {
+        SwapCollocatedVectorGrid(sameType);
+    }
+}
+
+template <size_t N>
+void CellCenteredVectorGrid<N>::Set(const CellCenteredVectorGrid& other)
+{
+    SetCollocatedVectorGrid(other);
+}
+
+template <size_t N>
+CellCenteredVectorGrid<N>& CellCenteredVectorGrid<N>::operator=(
+    const CellCenteredVectorGrid& other)
+{
+    Set(other);
+    return *this;
+}
+
+template <size_t N>
+void CellCenteredVectorGrid<N>::Fill(const Vector<double, N>& value,
+                                     ExecutionPolicy policy)
+{
+    Vector<size_t, N> size = DataSize();
+    auto acc = DataView();
+
+    ParallelForEachIndex(
+        Vector<size_t, N>(), size,
+        [&acc, &value](auto... indices) { acc(indices...) = value; }, policy);
+}
+
+template <size_t N>
+void CellCenteredVectorGrid<N>::Fill(
+    const std::function<Vector<double, N>(const Vector<double, N>&)>& func,
+    ExecutionPolicy policy)
+{
+    Vector<size_t, N> size = DataSize();
+    auto acc = DataView();
+    DataPositionFunc pos = DataPosition();
+
+    ParallelForEachIndex(
+        Vector<size_t, N>::MakeZero(), size,
+        [&func, &acc, &pos](auto... indices) {
+            acc(indices...) = func(pos(Vector<size_t, N>(indices...)));
+        },
+        policy);
+}
+
+template <size_t N>
+std::shared_ptr<VectorGrid<N>> CellCenteredVectorGrid<N>::Clone() const
+{
+    return std::shared_ptr<CellCenteredVectorGrid<N>>(
+        new CellCenteredVectorGrid<N>{ *this },
+        [](CellCenteredVectorGrid<N>* obj) { delete obj; });
+}
+
+template <size_t N>
+typename CellCenteredVectorGrid<N>::Builder
+CellCenteredVectorGrid<N>::GetBuilder()
+{
+    return Builder{};
+}
+
+template <size_t N>
+typename CellCenteredVectorGrid<N>::Builder&
+CellCenteredVectorGrid<N>::Builder::WithResolution(
+    const Vector<size_t, N>& resolution)
+{
+    m_resolution = resolution;
+    return *this;
+}
+
+template <size_t N>
+typename CellCenteredVectorGrid<N>::Builder&
+CellCenteredVectorGrid<N>::Builder::WithGridSpacing(
+    const Vector<double, N>& gridSpacing)
+{
+    m_gridSpacing = gridSpacing;
+    return *this;
+}
+
+template <size_t N>
+typename CellCenteredVectorGrid<N>::Builder&
+CellCenteredVectorGrid<N>::Builder::WithOrigin(
+    const Vector<double, N>& gridOrigin)
+{
+    m_gridOrigin = gridOrigin;
+    return *this;
+}
+
+template <size_t N>
+typename CellCenteredVectorGrid<N>::Builder&
+CellCenteredVectorGrid<N>::Builder::WithInitialValue(
+    const Vector<double, N>& initialVal)
+{
+    m_initialVal = initialVal;
+    return *this;
+}
+
+template <size_t N>
+CellCenteredVectorGrid<N> CellCenteredVectorGrid<N>::Builder::Build() const
+{
+    return CellCenteredVectorGrid{ m_resolution, m_gridSpacing, m_gridOrigin,
+                                   m_initialVal };
+}
+
+template <size_t N>
+std::shared_ptr<VectorGrid<N>> CellCenteredVectorGrid<N>::Builder::Build(
+    const Vector<size_t, N>& resolution, const Vector<double, N>& gridSpacing,
+    const Vector<double, N>& gridOrigin,
+    const Vector<double, N>& initialVal) const
+{
+    return std::shared_ptr<CellCenteredVectorGrid>(
+        new CellCenteredVectorGrid{ resolution, gridSpacing, gridOrigin,
+                                    initialVal },
+        [](CellCenteredVectorGrid* obj) { delete obj; });
+}
+
+template <size_t N>
+std::shared_ptr<CellCenteredVectorGrid<N>>
+CellCenteredVectorGrid<N>::Builder::MakeShared() const
+{
+    return std::shared_ptr<CellCenteredVectorGrid<N>>(
+        new CellCenteredVectorGrid{ m_resolution, m_gridSpacing, m_gridOrigin,
+                                    m_initialVal },
+        [](CellCenteredVectorGrid* obj) { delete obj; });
+}
+
+template class CellCenteredVectorGrid<2>;
+
+template class CellCenteredVectorGrid<3>;
+}  // namespace CubbyFlow
