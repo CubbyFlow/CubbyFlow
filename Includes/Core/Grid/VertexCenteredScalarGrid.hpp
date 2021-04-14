d7b915bae88ed22211c37656f1b25a5d9504f865
Chris Ohk <utilforever@gmail.com>
2021-04-14 16:31:36 +0900

    refactor: Create class 'VertexCenteredScalarGrid'

---
+++ Includes/Core/Grid/VertexCenteredScalarGrid.hpp
@@ -0,0 +1,142 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_VERTEX_CENTERED_SCALAR_GRID_HPP
+#define CUBBYFLOW_VERTEX_CENTERED_SCALAR_GRID_HPP
+
+#include <Core/Grid/ScalarGrid.hpp>
+
+namespace CubbyFlow
+{
+//!
+//! \brief N-D Vertex-centered scalar grid structure.
+//!
+//! This class represents N-D vertex-centered scalar grid which extends
+//! ScalarGrid. As its name suggests, the class defines the data point at the
+//! grid vertices (corners). Thus, A x B x ... grid resolution will have (A+1) x
+//! (B+1) x ... data points.
+//!
+template <size_t N>
+class VertexCenteredScalarGrid final : public ScalarGrid<N>
+{
+ public:
+    CUBBYFLOW_GRID_TYPE_NAME(VertexCenteredScalarGrid, N)
+
+    class Builder;
+
+    using ScalarGrid<N>::Resize;
+    using ScalarGrid<N>::Resolution;
+    using ScalarGrid<N>::Origin;
+
+    //! Constructs zero-sized grid.
+    VertexCenteredScalarGrid() = default;
+
+    //! Constructs a grid with given resolution, grid spacing, origin and
+    //! initial value.
+    VertexCenteredScalarGrid(
+        const Vector<size_t, N>& resolution,
+        const Vector<double, N>& gridSpacing =
+            Vector<double, N>::MakeConstant(1),
+        const Vector<double, N>& origin = Vector<double, N>{},
+        double initialValue = 0.0);
+
+    //! Copy constructor.
+    VertexCenteredScalarGrid(const VertexCenteredScalarGrid& other);
+
+    //! Returns the actual data point size.
+    Vector<size_t, N> DataSize() const override;
+
+    //! Returns data position for the grid point at (0, 0, ...).
+    //! Note that this is different from origin() since origin() returns
+    //! the lower corner point of the bounding box.
+    Vector<double, N> DataOrigin() const override;
+
+    //! Returns the copy of the grid instance.
+    std::shared_ptr<ScalarGrid<N>> Clone() const override;
+
+    //!
+    //! \brief Swaps the contents with the given \p other grid.
+    //!
+    //! This function swaps the contents of the grid instance with the given
+    //! grid object \p other only if \p other has the same type with this grid.
+    //!
+    void Swap(Grid<N>* other) override;
+
+    //! Sets the contents with the given \p other grid.
+    void Set(const VertexCenteredScalarGrid<N>& other);
+
+    //! Sets the contents with the given \p other grid.
+    VertexCenteredScalarGrid<N>& operator=(
+        const VertexCenteredScalarGrid<N>& other);
+
+    //! Returns builder fox VertexCenteredScalarGrid<N>.
+    static Builder GetBuilder();
+
+ protected:
+    using ScalarGrid<N>::SwapScalarGrid;
+    using ScalarGrid<N>::SetScalarGrid;
+};
+
+//! 2-D VertexCenteredScalarGrid type.
+using VertexCenteredScalarGrid2 = VertexCenteredScalarGrid<2>;
+
+//! 3-D VertexCenteredScalarGrid type.
+using VertexCenteredScalarGrid3 = VertexCenteredScalarGrid<3>;
+
+//! Shared pointer for the VertexCenteredScalarGrid2 type.
+using VertexCenteredScalarGrid2Ptr = std::shared_ptr<VertexCenteredScalarGrid2>;
+
+//! Shared pointer for the VertexCenteredScalarGrid3 type.
+using VertexCenteredScalarGrid3Ptr = std::shared_ptr<VertexCenteredScalarGrid3>;
+
+//!
+//! \brief Front-end to create VertexCenteredScalarGrid objects step by step.
+//!
+template <size_t N>
+class VertexCenteredScalarGrid<N>::Builder final : public ScalarGridBuilder<N>
+{
+ public:
+    //! Returns builder with resolution.
+    Builder& WithResolution(const Vector<size_t, N>& resolution);
+
+    //! Returns builder with grid spacing.
+    Builder& WithGridSpacing(const Vector<double, N>& gridSpacing);
+
+    //! Returns builder with grid origin.
+    Builder& WithOrigin(const Vector<double, N>& gridOrigin);
+
+    //! Returns builder with initial value.
+    Builder& WithInitialValue(double initialVal);
+
+    //! Builds VertexCenteredScalarGrid<N> instance.
+    VertexCenteredScalarGrid<N> Build() const;
+
+    //! Builds shared pointer of VertexCenteredScalarGrid<N> instance.
+    std::shared_ptr<VertexCenteredScalarGrid<N>> MakeShared() const;
+
+    //!
+    //! \brief Builds shared pointer of VertexCenteredScalarGrid<N> instance.
+    //!
+    //! This is an overriding function that implements ScalarGridBuilder2.
+    //!
+    std::shared_ptr<ScalarGrid<N>> Build(const Vector<size_t, N>& resolution,
+                                         const Vector<double, N>& gridSpacing,
+                                         const Vector<double, N>& gridOrigin,
+                                         double initialVal) const override;
+
+ private:
+    Vector<size_t, N> m_resolution = Vector<size_t, N>::MakeConstant(1);
+    Vector<double, N> m_gridSpacing = Vector<double, N>::MakeConstant(1.0);
+    Vector<double, N> m_gridOrigin;
+    double m_initialVal = 0.0;
+};
+}  // namespace CubbyFlow
+
+#endif
---
+++ Sources/Core/Grid/VertexCenteredScalarGrid.cpp
@@ -0,0 +1,152 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Core/Grid/VertexCenteredScalarGrid.hpp>
+
+namespace CubbyFlow
+{
+template <size_t N>
+VertexCenteredScalarGrid<N>::VertexCenteredScalarGrid(
+    const Vector<size_t, N>& resolution, const Vector<double, N>& gridSpacing,
+    const Vector<double, N>& origin, double initialValue)
+{
+    Resize(resolution, gridSpacing, origin, initialValue);
+}
+
+template <size_t N>
+VertexCenteredScalarGrid<N>::VertexCenteredScalarGrid(
+    const VertexCenteredScalarGrid& other)
+{
+    Set(other);
+}
+
+template <size_t N>
+Vector<size_t, N> VertexCenteredScalarGrid<N>::DataSize() const
+{
+    if (Resolution() != Vector<size_t, N>{})
+    {
+        return Resolution() + Vector<size_t, N>::MakeConstant(1);
+    }
+
+    return Vector<size_t, N>{};
+}
+
+template <size_t N>
+Vector<double, N> VertexCenteredScalarGrid<N>::DataOrigin() const
+{
+    return Origin();
+}
+
+template <size_t N>
+std::shared_ptr<ScalarGrid<N>> VertexCenteredScalarGrid<N>::Clone() const
+{
+    return std::shared_ptr<VertexCenteredScalarGrid<N>>(
+        new VertexCenteredScalarGrid<N>{ *this },
+        [](VertexCenteredScalarGrid<N>* obj) { delete obj; });
+}
+
+template <size_t N>
+void VertexCenteredScalarGrid<N>::Swap(Grid<N>* other)
+{
+    if (auto sameType = dynamic_cast<VertexCenteredScalarGrid*>(other);
+        sameType)
+    {
+        SwapScalarGrid(sameType);
+    }
+}
+
+template <size_t N>
+void VertexCenteredScalarGrid<N>::Set(const VertexCenteredScalarGrid& other)
+{
+    SetScalarGrid(other);
+}
+
+template <size_t N>
+VertexCenteredScalarGrid<N>& VertexCenteredScalarGrid<N>::operator=(
+    const VertexCenteredScalarGrid& other)
+{
+    Set(other);
+    return *this;
+}
+
+template <size_t N>
+typename VertexCenteredScalarGrid<N>::Builder
+VertexCenteredScalarGrid<N>::GetBuilder()
+{
+    return Builder{};
+}
+
+template <size_t N>
+typename VertexCenteredScalarGrid<N>::Builder&
+VertexCenteredScalarGrid<N>::Builder::WithResolution(
+    const Vector<size_t, N>& resolution)
+{
+    m_resolution = resolution;
+    return *this;
+}
+
+template <size_t N>
+typename VertexCenteredScalarGrid<N>::Builder&
+VertexCenteredScalarGrid<N>::Builder::WithGridSpacing(
+    const Vector<double, N>& gridSpacing)
+{
+    m_gridSpacing = gridSpacing;
+    return *this;
+}
+
+template <size_t N>
+typename VertexCenteredScalarGrid<N>::Builder&
+VertexCenteredScalarGrid<N>::Builder::WithOrigin(
+    const Vector<double, N>& gridOrigin)
+{
+    m_gridOrigin = gridOrigin;
+    return *this;
+}
+
+template <size_t N>
+typename VertexCenteredScalarGrid<N>::Builder&
+VertexCenteredScalarGrid<N>::Builder::WithInitialValue(double initialVal)
+{
+    m_initialVal = initialVal;
+    return *this;
+}
+
+template <size_t N>
+VertexCenteredScalarGrid<N> VertexCenteredScalarGrid<N>::Builder::Build() const
+{
+    return VertexCenteredScalarGrid{ m_resolution, m_gridSpacing, m_gridOrigin,
+                                     m_initialVal };
+}
+
+template <size_t N>
+std::shared_ptr<VertexCenteredScalarGrid<N>>
+VertexCenteredScalarGrid<N>::Builder::MakeShared() const
+{
+    return std::shared_ptr<VertexCenteredScalarGrid>(
+        new VertexCenteredScalarGrid{ m_resolution, m_gridSpacing, m_gridOrigin,
+                                      m_initialVal },
+        [](VertexCenteredScalarGrid* obj) { delete obj; });
+}
+
+template <size_t N>
+std::shared_ptr<ScalarGrid<N>> VertexCenteredScalarGrid<N>::Builder::Build(
+    const Vector<size_t, N>& resolution, const Vector<double, N>& gridSpacing,
+    const Vector<double, N>& gridOrigin, double initialVal) const
+{
+    return std::shared_ptr<VertexCenteredScalarGrid>(
+        new VertexCenteredScalarGrid{ resolution, gridSpacing, gridOrigin,
+                                      initialVal },
+        [](VertexCenteredScalarGrid* obj) { delete obj; });
+}
+
+template class VertexCenteredScalarGrid<2>;
+
+template class VertexCenteredScalarGrid<3>;
+}  // namespace CubbyFlow
