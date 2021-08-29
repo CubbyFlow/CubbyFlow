341e62a618d265224a9f01f7790715c1ae8eaeae
Chris Ohk <utilforever@gmail.com>
2021-08-29 23:53:28 +0900

    feat(gpu-v3): Create class 'CUDAPCISPHSolver3Example'

---
+++ Examples/CUDASPHSim/CUDAPCISPHSolver3Example.hpp
@@ -0,0 +1,27 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYFLOW_CUDA_PCISPH_SOLVER3_EXAMPLE_HPP
+#define CUBBYFLOW_CUDA_PCISPH_SOLVER3_EXAMPLE_HPP
+
+#include "SPHSimExample.hpp"
+
+namespace CubbyFlow
+{
+class CUDAPCISPHSolver3Example : public SPHSimExample
+{
+ public:
+    void RunExample(const std::string& rootDir, float targetSpacing,
+                    int numberOfFrames, const std::string& format,
+                    double fps) override;
+};
+}  // namespace CubbyFlow
+
+#endif
