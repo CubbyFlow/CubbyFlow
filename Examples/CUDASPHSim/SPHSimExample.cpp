60c6e485a0b1aeecc180cf5dbf3b6326a94a2e0d
Chris Ohk <utilforever@gmail.com>
2021-08-29 23:52:45 +0900

    feat(gpu-v3): Create class 'SPHSimExample'

---
+++ Examples/CUDASPHSim/SPHSimExample.cpp
@@ -0,0 +1,24 @@
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "SPHSimExample.hpp"
+
+using namespace CubbyFlow;
+
+void SPHSimExample::RunExample(const std::string& rootDir, float targetSpacing,
+                               int numberOfFrames, const std::string& format,
+                               double fps)
+{
+    (void)rootDir;
+    (void)targetSpacing;
+    (void)numberOfFrames;
+    (void)format;
+    (void)fps;
+}
