3b687c45edd59087e39a93b4d55775cde0ed347c
Chris Ohk <utilforever@gmail.com>
2021-12-04 17:24:51 +0900

    feat(doctest): Replace library 'googletest' with 'doctest'

--- Tests/UnitTests/AnimationTests.cpp
+++ Tests/UnitTests/AnimationTests.cpp
@@ -1,26 +1,36 @@
-#include "gtest/gtest.h"
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "doctest_proxy.hpp"
 
 #include <Core/Animation/Animation.hpp>
 
 using namespace CubbyFlow;
 
-TEST(Frame, Constructors)
+TEST_CASE("[Frame] - Constructors")
 {
     Frame frame;
-    EXPECT_EQ(0, frame.index);
-    EXPECT_DOUBLE_EQ(1.0 / 60.0, frame.timeIntervalInSeconds);
+    CHECK_EQ(0, frame.index);
+    CHECK_EQ(doctest::Approx(1.0 / 60.0), frame.timeIntervalInSeconds);
 }
 
-TEST(Frame, TimeInSeconds)
+TEST_CASE("[Frame] - TimeInSeconds")
 {
     Frame frame;
 
     frame.index = 180;
 
-    EXPECT_DOUBLE_EQ(3.0, frame.TimeInSeconds());
+    CHECK_EQ(doctest::Approx(3.0), frame.TimeInSeconds());
 }
 
-TEST(Frame, Advance)
+TEST_CASE("[Frame] - Advance")
 {
     Frame frame;
 
@@ -31,15 +41,15 @@ TEST(Frame, Advance)
         frame.Advance();
     }
 
-    EXPECT_EQ(54, frame.index);
+    CHECK_EQ(54, frame.index);
 
     frame.Advance(23);
 
-    EXPECT_EQ(77, frame.index);
+    CHECK_EQ(77, frame.index);
 
-    EXPECT_EQ(78, (++frame).index);
+    CHECK_EQ(78, (++frame).index);
 
-    EXPECT_EQ(78, (frame++).index);
+    CHECK_EQ(78, (frame++).index);
 
-    EXPECT_EQ(79, frame.index);
+    CHECK_EQ(79, frame.index);
 }
