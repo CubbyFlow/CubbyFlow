2fdd2e0f75e5785308455cfaf2bdf5a17e6429e1
Chris Ohk <utilforever@gmail.com>
2021-12-20 21:54:40 +0900

    feat(doctest): Replace library 'googletest' with 'doctest'

--- Tests/UnitTests/APICSolver2Tests.cpp
+++ Tests/UnitTests/APICSolver2Tests.cpp
@@ -1,10 +1,20 @@
-#include "gtest/gtest.h"
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "doctest_proxy.hpp"
 
 #include <Core/Solver/Hybrid/APIC/APICSolver2.hpp>
 
 using namespace CubbyFlow;
 
-TEST(APICSolver2, UpdateEmpty)
+TEST_CASE("[APICSolver2] - UpdateEmpty")
 {
     APICSolver2 solver;
 
--- Tests/UnitTests/APICSolver3Tests.cpp
+++ Tests/UnitTests/APICSolver3Tests.cpp
@@ -1,10 +1,19 @@
-#include "gtest/gtest.h"
+// This code is based on Jet framework.
+// Copyright (c) 2018 Doyub Kim
+// CubbyFlow is voxel-based fluid simulation engine for computer games.
+// Copyright (c) 2020 CubbyFlow Team
+// Core Part: Chris Ohk, Junwoo Hwang, Jihong Sin, Seungwoo Yoo
+// AI Part: Dongheon Cho, Minseo Kim
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
 
+#include "doctest_proxy.hpp"
 #include <Core/Solver/Hybrid/APIC/APICSolver3.hpp>
 
 using namespace CubbyFlow;
 
-TEST(APICSolver3, UpdateEmpty)
+TEST_CASE("[APICSolver3] - UpdateEmpty")
 {
     APICSolver3 solver;
 
